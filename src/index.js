33daa786a5107f1c6dc51c7949abad8f40b9a153
fatihdemirag <f2000fatih@hotmail.com>
2024-01-25 21:30:47 +0300

    düzeltmeler yapildi

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/authContext';
 import { Provider } from 'react-redux';
+import { configureStore } from './store/configureStore';
 
-const store = createStore();
+const store = configureStore();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
